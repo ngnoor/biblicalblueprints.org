23b488c8597a343dde2c04bcb8120992e6a843a4
Josh Duff <me@JoshDuff.com>
2018-03-16 13:12:22 -0500

    An htaccess file generator

---
+++ generator/generate-htaccess.js
@@ -0,0 +1,44 @@
+const redirects = require(`./redirects.json`)
+const fs = require(`fs`)
+
+const relative = path => require(`path`).join(__dirname, path)
+const singleRedirectRule = (from, to) => `
+RewriteCond %{REQUEST_URI} ^${ trimTrailingSlash(from) }(/|$)
+RewriteRule (.*) "${ to }" [R=301]
+`
+const trimTrailingSlash = url => url[url.length - 1] === `/`
+	? url.slice(0, -1)
+	: url
+
+main()
+
+function main() {
+	fs.writeFileSync(relative(`../public/.htaccess`), generate(redirects))
+}
+
+
+function generate(redirects) {
+	return `
+AddOutputFilterByType DEFLATE text/html text/plain text/css
+
+ExpiresActive On
+ExpiresDefault "access plus 4 hours"
+ExpiresByType text/html A90
+ExpiresByType text/plain A3600
+ExpiresByType text/css A3600
+
+<IfModule mod_rewrite.c>
+RewriteEngine On
+
+RewriteEngine On
+RewriteCond %{HTTPS} !=on
+RewriteCond %{HTTP_HOST} !localhost [NC]
+RewriteCond %{HTTP_HOST} !192.168 [NC]
+RewriteRule ^(.*) https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+
+RewriteBase /
+
+${ redirects.map(([ from, to ]) => singleRedirectRule(from, to)).join(``) }
+`
+}
+
