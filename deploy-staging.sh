6f6f5fdbbf2528a30147edec96ad6da7266c907d
Josh Duff <me@JoshDuff.com>
2018-03-16 15:48:24 -0500

    A staging deployment script

---
+++ deploy-staging.sh
@@ -0,0 +1,7 @@
+npm run build
+mkdir -p staging-deploy
+cp -r public/* staging-deploy
+cp public/.htaccess staging-deploy
+cp staging-robots.txt staging-deploy/robots.txt
+scp -r ./staging-deploy/. bibblue546832154@biblicalblueprints.org:/home/bibblue546832154/staging.biblicalblueprints.org
+rm -rf staging-deploy
