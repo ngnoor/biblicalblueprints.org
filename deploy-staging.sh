55dd0423f8eba9956ad5093d7b5387cacdc738bb
Josh Duff <me@JoshDuff.com>
2018-03-19 11:05:22 -0500

    Deploy with rsync

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -3,5 +3,5 @@ mkdir -p staging-deploy
 cp -r public/* staging-deploy
 cp public/.htaccess staging-deploy
 cp staging-robots.txt staging-deploy/robots.txt
-scp -r ./staging-deploy/. bibblue546832154@biblicalblueprints.org:/home/bibblue546832154/staging.biblicalblueprints.org
+rsync -e "/usr/bin/ssh"  --bwlimit=2000 -avzi --delete --include='*/' --include='.htaccess' ./staging-deploy/. bibblue546832154@biblicalblueprints.org:/home/bibblue546832154/staging.biblicalblueprints.org
 rm -rf staging-deploy
