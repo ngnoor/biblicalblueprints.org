350c16b68b2965d082c694876c14d04ab4a27ef7
DJTuck <69275810+DJTuck@users.noreply.github.com>
2020-09-14 11:31:43 -0500

    Update Contact.svelte
    Added a bio for Dr. K and a list of Board Members for DI. This page should probably be renamed "About", but I wasn't sure how to do that, and I didn't want to mess anything up.

--- pages/Contact.svelte
+++ pages/Contact.svelte
@@ -2,27 +2,28 @@
 	<title>Contact · Biblical Blueprints</title>
 </svelte:head>
 
-<h1>Contact Biblical Blueprints</h1>
+<h1>About</h1>
 
 <hr>
 
 <div class="contact-box">
-	<h2>Mailing address</h2>
-	<address>
-		Biblical Blueprints<br>
-		5858 Wenninghoff Rd., Suite 8<br>
-		Omaha, NE 68134-1911
-	</address>
+	<h2>President</h2>
+	<!-- Insert image of Dr. K from the front page, possibly a little larger and probably aligned on the left. Ultimately a design question, which is above my pay grade. -->
+	Phillip Kayser has degrees in education, theology, and philosophy, receiving an M.Div from Westminster Theological Seminary and a Ph.D from Whitefield Theological Seminary. Ordained in 1987, he serves as Senior Pastor of Dominion Covenant Church, a Presbyterian (CPC) church in Omaha, Nebraska. He has been privileged to speak at conferences hosted by the National Center For Family-Integrated Churches and Generations.org, and has spoken at three presidential campaign rallies.
+
+Professionally, he serves as Professor of Ethics at Whitefield Theological Seminary, is a board member for the Pickering Foundation of Biblical Preservation, and has lectured at the University of Nebraska at Omaha. He carefully defended Biblical inerrancy in his book, *The Canon of Scripture: A Presuppositional Study*, and has written over a dozen other books dealing with difficult theological subjects. Many of these works are being translated into foreign languages to equip the worldwide church.
+
+After traveling internationally for several years offering leadership training, worldview classes, and theological lectures, he founded Biblical Blueprints LLC (link) in 2020 to better advance his commitment to take the transformational gospel to the ends of the earth. He and his wife, Kathy, have five married children and fifteen grandchildren. In his spare time he enjoys talking theology with his friends and answering a few of his million emails. 
+
+His sermons, commentary, and books are available at <a href="biblicalblueprints.com">BiblicalBlueprints.com.
 </div>
 
 <div class="contact-box">
-	<h2>Phone</h2>
-	402-991-2421
-</div>
-
-<div class="contact-box">
-	<h2>Email</h2>
-	<a href="mailto:info@biblicalblueprints.org">info@biblicalblueprints.org</a>
+	<h2>The Board</h2>
+	Joel Kayser, Vice President
+	Kevin DeGroot, Treasurer
+	David Denekas, Secretary 
+	Gary Duff, Board Member 
 </div>
 
 <style>
